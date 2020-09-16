4c432661a084bdff726dc8d032f363b7a7410cae
Mick Styers <68083989+mqqk@users.noreply.github.com>
2020-09-16 02:30:54 -0400

    fileruploader example

---
+++ src/SearchBox/SearchBox.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './SearchBox.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+class SearchBox extends Component {
+    render () {
+        return (
+            <div className="SearchBox">
+                <FontAwesomeIcon icon={faSearch}/>
+                <input placeholder="Search Term" value={this.props.searchTerm} onChange={e => this.props.handleUpdate(e.target.value)}/>
+            </div>
+        )
+    }
+}
+
+export default SearchBox;
